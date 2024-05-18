b20efdef127e98d23871d44f1985bb3eb96db7ce
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-05-18 22:22:26 +0500

    4-oy 2-dars vazifa

---
+++ index.js
@@ -0,0 +1,62 @@
+// 1-masala
+
+// let a=+prompt("Son kiriting");
+// if(a>0){
+//     a++;
+// }
+// console.log(a);
+
+
+
+
+// 2-masala
+
+// let a=+prompt("Ixtiyoriy son kiriting")
+// if(a>0){
+//     a++;
+// }else{
+//     a-=2;
+// }
+// console.log(a);
+
+
+
+
+
+// 3-masala
+
+// let a=+prompt("Son kiriting");
+// if(a>0){
+//     a++;
+// }
+// else if(a<0){
+//     a-=2;
+// } else{
+//     a=10;
+// }
+// console.log(a);
+
+
+
+
+// 4-masala
+
+// let a=+prompt("1-sonni kiriting");
+// let b=+prompt("2-sonni kiriting");
+// let c=+prompt("3-sonni kiriting");
+// let sum=0;
+// if(a>0){
+//     sum++;
+// }else if(b>0){
+//     sum++;
+// } else if(c>0){
+//     sum++;
+// }
+// console.log(sum);
+
+
+
+
+
+// 5-masala
+
